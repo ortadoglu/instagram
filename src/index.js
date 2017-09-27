a400f87fc948440ca2a16c10237333de247410a7
ortadoglu <mircea.pienar@stefanini.com>
2017-09-27 09:30:01 +0300

    Move text to external file

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css'
 
 import { initialize, addTranslation } from 'react-localize-redux';
+import texts from './languageFiles/global.json';
 
 
 let store = createStore(galleryApp);
@@ -15,19 +16,8 @@ let store = createStore(galleryApp);
 const languages = ['en', 'de', 'ro'];
 store.dispatch(initialize(languages, { defaultLanguage: 'en' }));
 
-const json = {
-    "tag": ["Tag","Der Tag","Tag-ul"],
-    "description": ["Description","Beschreibung", "Descrierea"],
-    "location": ["Location", "Standort", "Locatie"],
-    "date": ["Date", "Datum", "Data"],  
-    "Image Url": ["Image URL", "Bild Url", "Url imagine"],
-    "add image": ["ADD IMAGE", "HINZUFÜGEN", "ADAUGA"],
-    "DISCLAIMER": ["This tab doesnt close so you cand watch language switching",
-        "Diese Registerkarte schließt nicht so, dass Sie die Sprache wechseln",
-        "Fereasta nu se inchide pentru a putea vedea schimbarea limbii"],
-  };
   
-store.dispatch(addTranslation(json));
+store.dispatch(addTranslation(texts));
 
 
 render(
